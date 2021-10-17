d272317f34e1ca4a65f6a79f068a4139cae64039
toneofgrey <62805173+toneofgrey@users.noreply.github.com>
2021-10-17 18:22:01 -0400

    new PR
    Fixed some of the things talked about in the last PR while adding functionality of a driver and a primitive menubar.

---
+++ driver.py
@@ -0,0 +1,16 @@
+"""
+driver.py:  Driver for SRHMD.
+TODOS:      Depends on what other things we will implement.
+Description:This file will mainly be responsive for calling out GUI for now.
+"""
+
+
+from gui import Gui as gui
+
+
+def main():
+    g = gui()
+
+
+if __name__ == "__main__":
+    main()
--- gui.py
+++ gui.py
@@ -1,37 +1,59 @@
-import numpy as np
-from tkinter import * 
-from tkinter.ttk import *
-from tkinter import ttk
+"""
+gui.py:     Main file for our Graphical User Interface.
+TODOS:      GUI quit function and Update function.
+Description:This file implement majority of the main functionalities of our GUI object,
+            such as initialization and update functions.
+"""
+
+
 from collapsiblepane import CollapsiblePane as cp
-from ttkthemes import ThemedStyle, ThemedTk
 from infodisplay import InfoDisplay as infd
 import json
+import numpy as np
+from tkinter import * 
+from ttkthemes import ThemedTk
 
-def makeCpane(name, data):
-    cpane = cp(root, name, name)
-    cpane.grid(row = 0, column = 0, sticky = 'w')
-    d = infd(cpane.frame, data).grid(row = 1, column = 0,)
-    return cpane
- 
-# Making root window or parent window
-root = ThemedTk(theme="equilux")
-root.geometry('1000x1000')
-root.configure(bg='#464646')
-root.title("SRHMD")
-Grid.columnconfigure(root, 0, weight=1)
 
-#m_display = Frame(root)
+class Gui():
+    def __init__(self):
+        self.root = self.intialization()
+        while(1):
+            self.root.update()
+        
 
-with open('config_example.json') as f:
-   data = json.load(f)
+    def makeCpane(self, name, data):
+        cpane = cp(self.root, name, name)
+        cpane.grid(row = 0, column = 0, sticky = 'w')
+        d = infd(cpane.frame, data).grid(row = 1, column = 0,)
+        return cpane
 
-list_of_machines = data["mlist"]
+    def load_file(self):
+        with open('config_example.json') as f:
+            data = json.load(f)
+        list_of_machines = data["mlist"]
+        Buttons = [None] * len(list_of_machines)
+        for i in range(0, len(list_of_machines)):
+            Buttons[i] = self.makeCpane(list_of_machines[i]['name'], list_of_machines[i])
+            Buttons[i].grid(row=i, column=0, sticky='nsew')
 
-	
-Buttons = [None] * len(list_of_machines)
-for i in range(0, len(list_of_machines)):
-    Buttons[i] = makeCpane(list_of_machines[i]['name'], list_of_machines[i])
-    Buttons[i].grid(row=i, column=0, sticky='nsew')
+    #
+    #def gui_exit(self):
+    #    self.root.quit
 
-#--------main
-root.mainloop()
+
+    # Making root window or parent window
+    def intialization(self):
+        root = ThemedTk(theme="equilux")
+        root.geometry('1000x1000')
+        root.configure(bg='#464646')
+        root.title("SRHMD")
+        Grid.columnconfigure(root, 0, weight=1)
+        menubar = Menu(root)
+        filemenu = Menu(menubar, tearoff=0)
+        filemenu.add_command(label="Load", command=lambda: self.load_file())
+        menubar.add_cascade(label="File", menu=filemenu)
+        filemenu.add_separator()
+        #filemenu.add_command(label="Exit", command=lambda: self.gui_exit())
+        root.config(menu=menubar)
+        return root
+
