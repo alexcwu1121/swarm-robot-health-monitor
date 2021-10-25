6b420c0ec0db3e911aecaea7c66262b466345f87
goedkene <evgoedken@gmail.com>
2021-10-25 17:11:52 -0400

    ..

--- dumbSendAndRec/dumbRec.py
+++ dumbSendAndRec/dumbRec.py
@@ -17,7 +17,7 @@ class TestReciever:
         if len(sys.argv) == 3:
             self.port_num = sys.argv[1]
             self.topic_name = sys.argv[2]
-        self.comms.add_subscriber_port('192.168.1.233',self.port_num,self.topic_name)
+        self.comms.add_subscriber_port('192.168.56.1',self.port_num,self.topic_name)
         time.sleep(.1)
 
     def run(self):
