d02c9042a16de2436bb5bf3cbe87ccbe0ac72d14
Daniel Wisdom <dwisdom751@gmail.com>
2021-10-28 17:57:33 -0400

    made display capable of recieving messages and changing the state using information from the message

--- display.py
+++ display.py
@@ -6,6 +6,7 @@ import multiprocessing as mp
 import os
 import random
 import sys
+import threading
 import time
 
 
@@ -13,14 +14,40 @@ class display_base:
     def __init__(self, fileN):
         self.comms = Comms()
         m_list = json.loads(open(fileN).read())
-        
-        portN = 4000
+        self.state = dict()
+        port_number = 4000
         
         for item in m_list['mlist']:
-            print(item)
-            self.comms.add_subscriber_port(item['ip'],str(portN),item['name'])
+            self.comms.add_subscriber_port(item['ip'],item['port'],item['ip'])
+            self.state[item['ip']] = dict()
+            for key in item['data'].keys():
+                self.state[item['ip']][key] = 0
+        
+        print(self.state)
+
+    def keep_state(self,ip):
+        print("here")
+        while True:
+            msg_recv = self.comms.get(ip)
+            if msg_recv is not None:
+                for key in msg_recv.payload.keys():
+                    self.state[ip][key] = [msg_recv.payload[key]]
+                print(self.state[ip])
+
+    def run_display(self):
+        rec_threads = list()
+        for ip in self.state.keys():
+            print(ip)
+            rec_threads.append(threading.Thread(target=self.keep_state,args=(ip,)))
+
+        for thr in rec_threads:
+            thr.start()
+
+        for thr in rec_threads:
+            thr.join()
 
 
 if __name__ == "__main__":
     my_rec = display_base(sys.argv[1])
+    my_rec.run_display()
     
