a5576141eec2c16c4cdd8a5d8ac2e39338dba18d
Nichm0617 <nicholasmataczynski0617@gmail.com>
2021-11-13 20:44:44 -0500

    inital animatedplot class

---
+++ gui/animatedplot.py
@@ -0,0 +1,40 @@
+import tkinter
+from tkinter import ttk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib import pyplot as plt
+import numpy as np
+import random
+
+#creates a plot that can be displayed withing the GUI
+#The root param is the root 
+#The seconds param is for how long in the past it should track, this is the range of the x axis
+#Min and max determin range of y axis
+class animatedplot(ttk.Frame):
+    def __init__(self, parent, seconds, min, max):
+        ttk.Frame.__init__(self, parent)    
+        plt.rcParams["figure.figsize"] = [3.00, 1.50]
+        plt.rcParams["figure.autolayout"] = True
+
+        plt.axes(xlim=(-seconds, 0), ylim=(min, max))
+        self.fig = plt.Figure(dpi=100)
+        ax = self.fig.add_subplot(xlim=(-seconds, 0), ylim=(min, max))
+        self.line, = ax.plot([], [], lw=2)
+
+        self.canvas = FigureCanvasTkAgg(self.fig, master=parent)
+        self.canvas.draw()
+        self.canvas.get_tk_widget().grid(row = 0, 
+                                            column = 0, 
+                                            sticky='w')
+
+        self.seconds = seconds
+        self.data = [0] * seconds
+
+    #update the plot with a new value
+    def update(self, value):
+        self.data.pop(0)
+        self.data.append(value)
+
+        x = np.linspace(-self.seconds, 0, self.seconds)
+        y = self.data
+        self.line.set_data(x, y)
+        self.canvas.draw()
