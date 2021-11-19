380b4eb390736ae84655afcf7d3aa7c7705827b4
Yao Dong <18ydong@gmail.com>
2021-11-19 17:12:38 -0500

    Comments

--- gui/statuslight.py
+++ gui/statuslight.py
@@ -1,3 +1,9 @@
+"""
+StatusLight.py
+Implements a status to indicate robot status that:
+    - changes when a new status is inputed
+"""
+
 import tkinter as tk
 import os
 from PIL import ImageTk,Image
@@ -7,9 +13,22 @@ class StatusLight(ttk.Frame):
 
     def __init__(self, parent):
         ttk.Frame.__init__(self, parent)
+        """
+        Attributes:
+            - parent: Parent GUI frame
+                This holds Statuslight as a GUI
+            - canvas: Tkinter Canvas GUI
+                This will hold the image of the status light
+            - red: Tkinter Image
+                Image read from file red.png to be used in canvas
+            - yellow: Tkinter Image
+                Image read from file yellow.png to be used in canvas
+            - green: Tkinter Image
+                Image read from file green.png to be used in canvas
+            - grey: Tkinter Image
+                Image read from file grey.png to be used in canvas
+        """
         self.parent = parent
-        self.color = 'grey'
-
         self.canvas = Canvas(self, width = 20, height = 20)
         self.canvas.grid(row = 0, column = 0)
         self.red = ImageTk.PhotoImage(Image.open(os.path.dirname(os.path.abspath(__file__))+"/red.png"))
@@ -19,6 +38,14 @@ class StatusLight(ttk.Frame):
         self.canvas.create_image(20, 20, anchor='center', image=self.grey)
 
     def set_status(self, status):
+        """
+        Function that changes the displayed image to match status
+        :parameters:
+            - status String
+                String represents the current status of the robot
+        :return:
+            None
+        """
         self.canvas.delete("all")
         if(status=="disconnect"):
             self.canvas.create_image(20, 20, anchor='center', image=self.grey)
@@ -28,7 +55,5 @@ class StatusLight(ttk.Frame):
             self.canvas.create_image(20, 20, anchor='center', image=self.green)
         elif(status=="critical"):
             self.canvas.create_image(20, 20, anchor='center', image=self.red)
-    
-    def destroy(self) -> None:
-        return super().destroy()
+
     
