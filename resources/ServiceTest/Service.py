4b5b1506af754211358ce9a9a3436c2d321b3a5a
Daniel Wisdom <dwisdom751@gmail.com>
2021-12-04 20:11:35 -0500

    added black box testing in the resources folder

---
+++ resources/ServiceTest/Service.py
@@ -0,0 +1,44 @@
+"""
+Service.py:  Base class for server analytics
+Description: Base class for server analytics
+"""
+
+from abc import ABC, abstractmethod
+from comms import Comms
+
+class Service(ABC):
+    def __init__(self, service_conf):
+        """
+        Attributes:
+            - config: Dict<String:Float>
+                Dictionaries of options
+            - comms: Comms
+                Message queue manager
+            - state: Dict<String:Float>
+                State of each ingressed data field
+        """
+        self.config = dict()
+        self.state = dict()
+        self.comms = Comms()
+
+        self.init_config(service_conf)
+
+    @abstractmethod
+    def init_config(self):
+        """Initialize options from a configuration file"""
+        pass
+
+    @abstractmethod
+    def update_options(self):
+        """Update options from dispatcher messages"""
+        pass
+
+    @abstractmethod
+    def transform(self):
+        """Transform a data stream and publish"""
+        pass
+
+    @abstractmethod
+    def run(self):
+        """Initialize, establish connections, and launch"""
+        pass
---
+++ resources/ServiceTest/TimeoutAnalytic.py
@@ -0,0 +1,62 @@
+import sys
+import json
+import time
+from Service import Service
+from comms import Message
+
+class TimeoutAnalytic(Service):
+
+    def init_config(self, service_conf):
+        # TODO: don't hardcode this config file. get it from gui
+        with open("config/config_testing_c.json") as f:
+            self.config = json.load(f)
+
+        # TODO: don't hardcode this config file. get it from gui
+        with open("config/config_timeout.json") as f:
+            self.analytic_config = json.load(f)
+
+        self.timeout = float(self.analytic_config['Timeout']['timeout'])
+
+        # Set up subscribers according to config
+        for item in self.analytic_config['Timeout']['subscribe']:
+            self.comms.add_subscriber_port(item['ip'], item['port'], item['topic'])
+
+        # Set up publisher according to config
+        ip = self.analytic_config['Timeout']['publish']['ip']
+        port = self.analytic_config['Timeout']['publish']['port']
+        self.comms.add_publisher_port(ip, port, 'Timeout')
+
+        time.sleep(0.5)
+
+    def update_options(self):
+        pass
+
+    def transform(self):
+        """
+        self.state[item['ip']] = dict()
+        self.state[item['ip']]["Timeout"] = 0
+        self.state[item['ip']]["PrevTime"] = time.process_time()
+        """
+        for topic in self.comms.subscriber_ports.keys() - self.state.keys():
+            msg_recv = self.comms.get(topic)
+            if msg_recv is not None:
+                if msg_recv.topic not in self.state.keys():
+                    self.state[msg_recv.topic] = dict()
+                self.state[msg_recv.topic]["PrevTime"] = time.process_time()
+        return
+
+    def run(self):
+        while True:
+            # update state and transform data
+            self.transform()
+            for ip in self.state.keys():
+                time_elapsed = time.process_time() - self.state[ip]["PrevTime"]
+                timeout = {"Timeout": True}
+                if time_elapsed > self.timeout:
+                    timeout["Timeout"] = False
+                self.comms.send("Timeout", Message(ip, timeout))
+
+
+if __name__ == "__main__":
+    timeout = TimeoutAnalytic()
+    timeout.run()
